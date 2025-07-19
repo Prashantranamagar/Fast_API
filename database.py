48566a6b88723dad8129da0042baf3cbf0cc321f
Prashantranamagar <ranamagar.prashant@gmail.com>
2025-07-19 15:41:45 +0545

    setup database conneciton

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+"""
+
+create_engine: Establishes a connection to the database.
+
+sessionmaker: Factory for creating new Session objects (used to talk to the DB).
+
+declarative_base: Base class for defining your ORM models (tables).
+
+"""
+SQLALCHEMY_DATABASE_URL = "sqlite:///./books.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL , connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
