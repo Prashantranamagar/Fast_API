d831c8184001e665f112350904a4a30d1ebed3a4
Prashantranamagar <ranamagar.prashant@gmail.com>
2025-07-19 18:18:52 +0545

    change db url to sqlite

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ sessionmaker: Factory for creating new Session objects (used to talk to the DB).
 declarative_base: Base class for defining your ORM models (tables).
 
 """
-SQLALCHEMY_DATABASE_URL = "sqlite:///./books.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL , connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
